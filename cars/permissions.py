997fdcca374a686c02cc72da01038b7309d725f8
Lee-Roy King <leeroywking@gmail.com>
2020-10-14 18:50:10 -0700

    updated to use elephantsql

--- cars/permissions.py
+++ cars/permissions.py
@@ -3,8 +3,9 @@ from rest_framework import permissions
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         #read only
+
         if request.method in permissions.SAFE_METHODS:
             return True
         
-        #write only for author
-        return obj.owner == request.user
+        # #write only for author
+        # return obj.owner == request.user
--- jwt_django/settings.py
+++ jwt_django/settings.py
@@ -87,22 +87,22 @@ WSGI_APPLICATION = "jwt_django.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD":"postgres",
-#         "HOST": "db",
-#         "PORT":5432
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_USER"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD":env.str("DB_PASS"),
+        "HOST": env.str("DB_URL"),
+        "PORT":5432
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
